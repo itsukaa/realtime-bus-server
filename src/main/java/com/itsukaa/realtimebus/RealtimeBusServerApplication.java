58d71a615bf526fd76cf8ad3eda629b4f8e7179a
itsukaa <itsukaa@foxmail.com>
2021-04-18 20:11:03 +0800

    项目搭建

---
+++ src/main/java/com/itsukaa/realtimebus/RealtimeBusServerApplication.java
@@ -0,0 +1,13 @@
+package com.itsukaa.realtimebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeBusServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeBusServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsukaa/realtimebus/RealtimeBusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsukaa.realtimebus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeBusServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
